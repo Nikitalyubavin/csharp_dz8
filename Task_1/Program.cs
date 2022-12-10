c6e78506b6f11f835d9776db732a032f4b099e00
Nikita <mail@gmail.com>
2022-12-10 18:20:13 +0300

    Finished Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,84 @@
+﻿/* Задайте двумерный массив. Напишите программу, которая упорядочит
+по убыванию элементы каждой строки двумерного массива.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2 */
+
+Console.Clear();
+Console.Clear();
+int rows = UserInput("Введите количество строк: ", "Введено неверное значение!");
+int columns = UserInput("Введите количество столбцов: ", "Введено неверное значение!");
+int[,] array = ArrayOfRealNumbers(rows, columns, 0, 10);
+
+Console.WriteLine();
+PrintArray(array);
+
+Console.WriteLine();
+SortingInRowsArray(array);
+PrintArray(array);
+
+
+
+int[,] SortingInRowsArray(int[,] arr)
+{
+    int temp;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(1) - 1; k++)
+            {
+                if (arr[i, k] < arr[i, k + 1])
+                {
+                    temp = arr[i, k + 1];
+                    arr[i, k + 1] = arr[i, k];
+                    arr[i, k] = temp;
+                }
+            }
+            
+        }
+    }
+    return arr;
+}
+
+int[,] ArrayOfRealNumbers(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] arr = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            arr[i,j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int UserInput(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine()??"", out int number) && number > 0) return number;
+        else Console.WriteLine(errorMessage);
+    }
+}
